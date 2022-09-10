7a381bf730b5c0af614915e00ac3ba387b98d8d7
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2022-09-10 14:45:52 -0400

    working with example tables

--- e2e/dist/index.js
+++ e2e/dist/index.js
@@ -13,11 +13,13 @@ var _parseEnv = require("./env/parseEnv");
 
 var fs = _interopRequireWildcard(require("fs"));
 
+var _tagHelper = require("./support/tag-helper");
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
@@ -28,11 +30,11 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 var environment = (0, _parseEnv.env)('NODE_ENV');
 exports.environment = environment;
 
-_dotenv.default.config({
+_dotenv["default"].config({
   path: (0, _parseEnv.env)('COMMON_CONFIG_FILE')
 });
 
-_dotenv.default.config({
+_dotenv["default"].config({
   path: "".concat((0, _parseEnv.env)('ENV_PATH')).concat(environment, ".env")
 });
 
@@ -40,6 +42,17 @@ var hostsConfig = (0, _parseEnv.getJsonFromFile)((0, _parseEnv.env)('HOSTS_URL_P
 var pagesConfig = (0, _parseEnv.getJsonFromFile)((0, _parseEnv.env)('PAGES_URL_PATH'));
 var emailsConfig = (0, _parseEnv.getJsonFromFile)((0, _parseEnv.env)('EMAILS_URL_PATH'));
 var mappingFiles = fs.readdirSync("".concat(process.cwd()).concat((0, _parseEnv.env)('PAGE_ELEMENTS_PATH')));
+
+var getEnvList = function getEnvList() {
+  var envList = Object.keys(hostsConfig);
+
+  if (envList.length === 0) {
+    throw Error("\u2757\uFE0F No environments mapped in your ".concat((0, _parseEnv.env)('HOSTS_URL_PATH')));
+  }
+
+  return envList;
+};
+
 var pageElementMappings = mappingFiles.reduce(function (pageElementConfigAcc, file) {
   var key = file.replace('.json', '');
   var elementMappings = (0, _parseEnv.getJsonFromFile)("".concat((0, _parseEnv.env)('PAGE_ELEMENTS_PATH')).concat(file));
@@ -52,9 +65,9 @@ var worldParameters = {
   pageElementMappings: pageElementMappings
 };
 var common = "./src/features/**/*.feature                 --require-module ts-node/register                 --require ./src/step-definitions/**/*.ts                 --world-parameters ".concat(JSON.stringify(worldParameters), "                 -f json:./reports/report.json                 --format progress-bar                 --parallel ").concat((0, _parseEnv.env)('PARALLEL'), "                 --retry ").concat((0, _parseEnv.env)('RETRY'));
-var dev = "".concat(common, " --tags '@dev'");
+var dev = (0, _tagHelper.generateCucumberRuntimeTag)(common, environment, getEnvList(), 'dev');
 exports.dev = dev;
-var smoke = "".concat(common, " --tags '@smoke'");
+var smoke = (0, _tagHelper.generateCucumberRuntimeTag)(common, environment, getEnvList(), 'smoke');
 exports.smoke = smoke;
-var regression = "".concat(common, " --tags '@regression'");
+var regression = (0, _tagHelper.generateCucumberRuntimeTag)(common, environment, getEnvList(), 'regression');
 exports.regression = regression;
--- e2e/src/features/playground/login.feature
+++ e2e/src/features/playground/login.feature
@@ -1,12 +1,37 @@
 Feature: As a user I can interact with login forms
 
     @smoke @regression 
-    Scenario: As a user I can interact and assert on login forms with localhost creds
+    Scenario Outline: As a user I can interact and assert on login forms with localhost creds
         Given I am on the "home" page
         And I click the "playground" button
         When I am directed to the "playground" page
         And I fill in the "email" input with "$.TEST_EMAIL"
         And I fill in the "password" input with "$.TEST_PASSWORD"
         And the "email" should contain the value "admin@testingtalkshub.com.au"
-        And the "password" should contain the value "Password1234"
+        And the "password" should contain the value "<password>"
+
+    @localhost
+    Examples:
+        | password     |
+        | Password1234 | 
+
+    @production
+    Examples:
+        | password        |
+        | A4sf&lefje^yeio | 
+
+    
+    @smoke @regression
+    Scenario Outline: As a user I expect validation on the login input for an incorrect email   
+        Given I am on the "home" page
+        And I click the "playground" button
+        When I am directed to the "playground" page
+        And I fill in the "email" input with "<email>"
+        And I fill in the "password" input with "Password1234"
+        And the "email error" should contain the text "Please include an '@' in the email address."
+    
+    Examples:
+        | email            |
+        | com.testingtalks | 
+        | com.             |
 
